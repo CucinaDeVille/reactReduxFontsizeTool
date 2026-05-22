56d00cb4837fe0cdacfde3ccd3db408106917849
luca <luca.romano@t-online.de>
2026-05-22 13:04:13 +0200

    feat(wiki): :sparkles: query wiki api with user input

---
+++ src/fetchWiki.jsx
@@ -0,0 +1,34 @@
+import axios from "axios";
+
+function getTextFromAxios(responseObj) {
+
+    let str;
+
+    if (responseObj.query == undefined) return "---";
+
+    const keyVal = Object.keys(responseObj.query.pages);
+
+    str = responseObj.query.pages[keyVal].extract;
+
+    return str;
+}
+
+export function fetchWiki() {
+
+    return async (dispatch, getState) => {
+
+        const text = getState().inputText;
+
+        const response = await axios.get(
+            `https://de.wikipedia.org/w/api.php?action=query&origin=*&prop=extracts&format=json&titles=${text}`
+        );
+
+        const res = getTextFromAxios(response.data);
+
+        dispatch({
+            type: "SET_OUTPUT_TEXT",
+            payload: res
+        });
+
+    };
+}
