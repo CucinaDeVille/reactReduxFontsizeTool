039f125964d50bf76e5392bcefbc4800161cbe53
luca <luca.romano@t-online.de>
2026-05-22 11:45:48 +0200

    chore: :tada: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import MyInput from "./MyInput";
+import MyOutput from "./MyOutput";
+
+export default function App() {
+  return (
+    <div className="App">
+      <MyInput/>
+      <MyOutput/>
+    </div>
+  );
+}
---
+++ src/MyReset.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import './MyReset.css';
+
+export default function MyInput() {
+    const handleClick = () => {
+        alert("Alles zurücksetzen!")
+    }
+
+    return (
+        <div >
+            <button className="button" onClick={handleClick}>R</button>
+        </div>
+    );
+}
