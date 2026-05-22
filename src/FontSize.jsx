7477d869bfd5c2e6c15ba62faf474bc1fad5c6ed
luca <luca.romano@t-online.de>
2026-05-22 12:17:26 +0200

    feat(size): :sparkles: utilize variables from store

--- src/FontSize.jsx
+++ src/FontSize.jsx
@@ -1,15 +1,20 @@
-import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import './FontSize.css';
 
 export default function FontSize() {
-  const [num, setNum] = useState(16);
+
+  const dispatch = useDispatch();
+  const num = useSelector(state => state.size);
+
   return (
     <div>
       FontSize
       <div className='niceButtons'>
+
         <span>{num}</span>
-        <button className='button' onClick={() => setNum(num + 1)}>+</button>
-        <button className='button' onClick={() => setNum(num - 1)}>-</button>
+
+        <button className='button' onClick={() => dispatch ({type: "SET_SIZE", payload: num +1})}>+</button>
+        <button className='button' onClick={() => dispatch ({type: "SET_SIZE", payload: num -1})}>-</button>
       </div>
     </div>
   );
